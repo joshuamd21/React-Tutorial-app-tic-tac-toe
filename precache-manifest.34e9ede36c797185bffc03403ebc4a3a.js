bc71f977b10474c70a557d3fc518091f100982da
joshuamd21 <joshua.downey21@gmail.com>
2022-03-10 13:27:04 -0600

    Updates

---
+++ precache-manifest.34e9ede36c797185bffc03403ebc4a3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "618d43c4c700ae7b20f5",
+    "url": "/React-Tutorial-app-tic-tac-toe/static/css/main.815c8d3f.chunk.css"
+  },
+  {
+    "revision": "618d43c4c700ae7b20f5",
+    "url": "/React-Tutorial-app-tic-tac-toe/static/js/main.1c37073d.chunk.js"
+  },
+  {
+    "revision": "48db49469494b0f3b793",
+    "url": "/React-Tutorial-app-tic-tac-toe/static/js/runtime~main.5a97cdee.js"
+  },
+  {
+    "revision": "58518ca7a856dbc6c8b4",
+    "url": "/React-Tutorial-app-tic-tac-toe/static/js/2.185a0e82.chunk.js"
+  },
+  {
+    "revision": "12efbf530bca2aba8c8c8ae67e489232",
+    "url": "/React-Tutorial-app-tic-tac-toe/index.html"
+  }
+];
